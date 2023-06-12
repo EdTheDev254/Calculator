7b55625345a0af6f486603fc60c53c8712e1f723
Edwin Wagha <edwin.wagha20@gmail.com>
2023-06-12 16:04:20 +0300

    Created the window and set it up

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-# Test
+import tkinter as tk
+import tkinter.ttk as ttk
+
+
+class MyApp(tk.Tk):
+	def __init__(self):
+		super().__init__()
+		# Restrict window size from being changed
+		self.resizable(False, False)
+		# Set The Title
+		self.title('Calculator')
+		# Set the window size
+		self.geometry('300x400')
+
+
+if __name__ == '__main__':
+	app = MyApp()
+	app.mainloop()
