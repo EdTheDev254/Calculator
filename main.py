1b107973f8c76bb33c5b8af523448451dc81eb73
Edwin Wagha <edwin.wagha20@gmail.com>
2023-06-14 13:42:29 +0300

    Created the display window

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import tkinter as tk
 import tkinter.ttk as ttk
 
-
 class MyApp(tk.Tk):
 	def __init__(self):
 		super().__init__()
@@ -12,7 +11,27 @@ class MyApp(tk.Tk):
 		# Set the window size
 		self.geometry('300x400')
 
+		# Create frames for the button widgets
+		self.frame1 = tk.Frame(self)
+		self.frame2 = tk.Frame(self)
+		self.frame3 = tk.Frame(self)
+		self.frame4 = tk.Frame(self)
+		self.frame5 = tk.Frame(self)
 
+		# Place the frames on the display
+		self.frame1.grid(row=0, column=0)
+		self.frame2.grid(row=1, column=0)
+		self.frame3.grid(row=2, column=0)
+		self.frame4.grid(row=3, column=0)
+		self.frame5.grid(row=4, column=0)
+
+		# Create the display Entry
+		self.calculator_display = tk.Text(self, width=15, height=2, bg='green',fg='black', font=('courier', 21), bd=3)
+
+		# Place the display entry onto frame1
+		self.calculator_display.grid(row=0, column=0, padx=20, pady=10)
+		
+		
 if __name__ == '__main__':
 	app = MyApp()
 	app.mainloop()
